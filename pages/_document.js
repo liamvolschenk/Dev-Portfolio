6b6fe2f0631f2f5e0495816463bc9d89dfe11fdf
Liam Volschenk <liamvolschenk@gmail.com>
2021-08-17 11:43:32 +0200

    analytics

--- pages/_document.js
+++ pages/_document.js
@@ -5,23 +5,15 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    {/* Global Site Tag (gtag.js) - Google Analytics */}
-                    <script
-                        async
-                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-                    />
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-                        }}
-                    />
+
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-RNM6HQ1XXV"></script>
+                    <script>
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments)}
+                        gtag('js', new Date());
+
+                        gtag('config', 'G-RNM6HQ1XXV');
+                    </script>
                 </Head>
                 <body>
                     <Main />
